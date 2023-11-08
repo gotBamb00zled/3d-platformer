7744454dcd3bb057e2e6fb4e749ebaceb6f9dd54
Greg Klein <i.am.the.bamb00zled@gmail.com>
2023-11-07 22:08:40 -0700

    first commit...?

---
+++ Player.gd
@@ -0,0 +1,10 @@
+
+extends RigidBody3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	
+	apply_central_force(input * 1200.0 * delta)
